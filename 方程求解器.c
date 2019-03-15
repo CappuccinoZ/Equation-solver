f2b52fceef9a97522344c783cb65293f04cb6e30
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2019-03-15 22:18:22 +0800

    Add files via upload

--- 方程求解器.c
+++ 方程求解器.c
@@ -82,7 +82,7 @@ void fun3(double a, double b, double c, double d)//ax^3+bx^2+cx+d=0
 	else//d = 0
 	{
 		printf("\t0.000000000000\n");//x = 0
-		fun2(a, b, c);
+		fun2(a, b, c);//降次
 	}
 }
 
@@ -179,32 +179,86 @@ void fun4(double a, double b, double c, double d, double e)//ax^4+bx^3+cx^2+dx+e
 	}
 }
 
-void judgement(double a, double b, double c, double d, double e)//判断方程最高次数
+double fun5_subsidiary(double a, double b, double c, double d, double e, double x)//牛顿迭代法
 {
-	if (a)//四次
+	double derivative;
+	temp = x * (x*(x*(x + a) + b) + c) + d;
+	derivative = x * (x*(x*(5 * x + 4 * a) + 3 * b) + 2 * c) + d;
+	x = x - (temp * x + e) / derivative;
+	return x;
+}
+
+void fun5(double a, double b, double c, double d, double e)//x^5+ax^4+bx^3+cx^2+dx+e=0
+{
+	double x, i;
+	if (e)
+	{
+		if (d)
+		{
+			x = 0;
+		}
+		else if (5 + 4 * a + 3 * b + 2 * c + d)
+		{
+			x = 1;
+		}
+		else if (5 - 4 * a + 3 * b - 2 * c + d)
+		{
+			x = -1;
+		}
+		else if (80+32*a+12*b+4*c+d)
+		{
+			x = 2;
+		}
+		else//防止导数等于0
+		{
+			x = 2.718;
+		}
+
+		for (i = 0; i < 20; i++)
+		{
+			x = fun5_subsidiary(a, b, c, d, e, x);
+		}
+		printf("\t%.12lf\n", x);
+		fun4(1, x + a, x * x + a * x + b, x * x*x + a * x*x + b * x + c, temp);//降次
+	}
+	else
+	{
+		printf("\t0.000000000000\n");
+		fun4(1, a, b, c, d);
+	}
+}
+
+void judgement(double a, double b, double c, double d, double e, double f)//判断方程最高次数
+{
+	if (a)//五次
+	{
+		printf("\n\tx1,x2,x3,x4,x5:\n");
+		fun5(b / a, c / a, d / a, e / a, f / a);
+	}
+	else if (b)//四次
 	{
 		printf("\n\tx1,x2,x3,x4:\n");
-		fun4(a, b, c, d, e);
+		fun4(b, c, d, e, f);
 	}
-	else if (b)//三次
+	else if (c)//三次
 	{
 		printf("\n\tx1,x2,x3:\n");
-		fun3(b, c, d, e);
+		fun3(c, d, e, f);
 	}
-	else if (c)//二次
+	else if (d)//二次
 	{
 		printf("\n\tx1,x2:\n");
-		fun2(c, d, e);
+		fun2(d, e, f);
 	}
-	else if (d)//一次
+	else if (e)//一次
 	{
-		printf("\n\tx = %.12lf.\n", -e / d);
+		printf("\n\tx = %.12lf.\n", -f / e);
 	}
-	else if (e)//常值
+	else if (f)//常值
 	{
 		printf("\n\t无解\n");
 	}
-	else
+	else//常值
 	{
 		printf("\n\t任意复数\n");
 	}
@@ -212,20 +266,22 @@ void judgement(double a, double b, double c, double d, double e)//判断方程最高次
 
 int main(void)
 {
-	double a, b, c, d, e;
-	printf("求解方程ax^4+bx^3+cx^2+dx+e=0(系数均为实数):\n");
-	printf("请输入四次项系数:\n");
+	double a, b, c, d, e, f;
+	printf("求解方程ax^5+bx^4+cx^3+dx^2+ex+f=0(系数均为实数):\n");
+	printf("请输入五次项系数:\n");
 	scanf_s("%lf", &a);
-	printf("请输入三次项系数:\n");
+	printf("请输入四次项系数:\n");
 	scanf_s("%lf", &b);
-	printf("请输入二次项系数:\n");
+	printf("请输入三次项系数:\n");
 	scanf_s("%lf", &c);
-	printf("请输入一次项系数:\n");
+	printf("请输入二次项系数:\n");
 	scanf_s("%lf", &d);
-	printf("请输入常数项:\n");
+	printf("请输入一次项系数:\n");
 	scanf_s("%lf", &e);
+	printf("请输入常数项:\n");
+	scanf_s("%lf", &f);
 
-	judgement(a, b, c, d, e);
+	judgement(a, b, c, d, e, f);
 	_getch();
 	return 0;
 }
