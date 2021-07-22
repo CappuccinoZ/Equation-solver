02e243a7e4e7a9fa8876e90350fa190638bfac3e
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2021-07-22 18:47:36 +0800

    Add files via upload

--- complex.js
+++ complex.js
@@ -1,105 +1,185 @@
-/*complex.js|CappuccinoZ.github.io|2021-6-10*/
+/*complex.js|CappuccinoZ.github.io|2021-7-22*/
 class Complex {
     constructor(real, imag) {
         if (isNaN(real) || isNaN(imag)) throw new TypeError();
         this.r = real;
-        this.i = imag
+        this.i = imag;
     }
-    norm() {
-        return this.r * this.r + this.i * this.i
-    }
-    abs() {
-        return Math.hypot(this.r, this.i)
-    }
-    arg() {
-        return Math.atan2(this.i, this.r)
-    }
-    conj() {
-        return new Complex(this.r, -this.i)
-    }
-    neg() {
-        return new Complex(-this.r, -this.i)
-    }
+    abs2() { return this.r * this.r + this.i * this.i; }
+    abs() { return Math.hypot(this.r, this.i); }
+    arg() { return Math.atan2(this.i, this.r); }
+    conj() { return new Complex(this.r, -this.i); }
+    neg() { return new Complex(-this.r, -this.i); }
     add(that) {
         if (typeof that == "number") return new Complex(this.r + that, this.i);
-        return new Complex(this.r + that.r, this.i + that.i)
+        return new Complex(this.r + that.r, this.i + that.i);
     }
     sub(that) {
         if (typeof that == "number") return new Complex(this.r - that, this.i);
-        return new Complex(this.r - that.r, this.i - that.i)
+        return new Complex(this.r - that.r, this.i - that.i);
     }
     mul(that) {
         if (typeof that == "number") return new Complex(this.r * that, this.i * that);
-        return new Complex(this.r * that.r - this.i * that.i, this.r * that.i + this.i * that.r)
+        return new Complex(this.r * that.r - this.i * that.i, this.r * that.i + this.i * that.r);
     }
     div(that) {
         if (typeof that == "number") return new Complex(this.r / that, this.i / that);
-        return this.mul(that.conj()).div(that.norm())
-    }
-    log() {
-        return new Complex(Math.log(this.norm()) * 0.5, this.arg())
-    }
-    exp() {
-        return new Complex(Math.cos(this.i), Math.sin(this.i)).mul(Math.exp(this.r))
-    }
-    pow(that) {
-        if (typeof that == "number") return new Complex(Math.cos(this.arg() * that), Math.sin(this.arg() * that)).mul(Math.pow(this.abs(), that));
-        return this.log().mul(that).exp()
-    }
-    sqrt() {
-        return this.pow(0.5)
-    }
-    cbrt() {
-        if (equal(this.i, 0) && this.r < 0) return new Complex(Math.cbrt(this.r), 0);
-        return this.pow(1.0 / 3)
+        return this.mul(that.conj()).mul(1 / that.abs2());
     }
     toString() {
-        var a = Number(this.r.toFixed(12)),
-            b = Number(this.i.toFixed(12));
+        var a = Number(this.r.toFixed(12)), b = Number(this.i.toFixed(12));
         var str = "";
-        if (equal(a, 0) && equal(b, 0)) str = "0";
-        else {
-            if (a != 0) {
-                str += format(a);
-                if (b > 0) str += "+"
-            }
-            switch (b) {
-                case 0:
-                    break;
-                case -1:
-                    str += "-";
-                case 1:
-                    str += "i";
-                    break;
-                default:
-                    str += format(b) + "i";
-                    break
-            }
+        if (equal(b, 0)) return a.toString();
+        if (a != 0) {
+            str += a;
+            if (b > 0) str += "+";
         }
-        return str
+        switch (b) {
+            case -1: str += "-i"; break;
+            case 1: str += "i"; break;
+            default: str += b + "i"; break;
+        }
+        return str;
     }
 }
-const equal = (a, b) => Math.abs(a - b) <= 1e-12;
 
-function show(text) {
-    var str = (typeof text == "number") ? format(Number(text.toFixed(12))) : text.toString();
-    document.getElementById("box").innerHTML += (str + "\\\\")
+function equal(a, b) {
+    if (typeof a == "number") a = new Complex(a, 0);
+    if (typeof b == "number") b = new Complex(b, 0);
+    var t = a.sub(b);
+    return Math.abs(t.r) < 1E-12 && Math.abs(t.i) < 1E-12;
 }
 
+function sqrt(z) {
+    if (typeof z == "number") z = new Complex(z, 0);
+    return new Complex(Math.cos(z.arg() / 2), Math.sin(z.arg() / 2)).mul(Math.sqrt(z.abs()));
+}
+function cbrt(z) {
+    if (typeof z == "number") z = new Complex(z, 0);
+    var t = (equal(z.i, 0) && z.r < 0) ? Math.PI : z.arg() / 3;
+    return new Complex(Math.cos(t), Math.sin(t)).mul(Math.pow(z.abs(), 1.0 / 3));
+}
+
+function fun2(a, b, c) {
+    if (typeof a == "number") a = new Complex(a, 0);
+    if (typeof b == "number") b = new Complex(b, 0);
+    if (typeof c == "number") c = new Complex(c, 0);
+    var t = sqrt(b.mul(b).sub(a.mul(c).mul(4)));
+    show(b.neg().add(t).div(a.mul(2)));
+    show(b.neg().sub(t).div(a.mul(2)));
+}
+
+function fun3_root(a, b, c, d) {
+    var k = 1 / a;
+    a = b * k;
+    b = c * k;
+    c = d * k;
+    var p = b - a * a / 3, q = (2 * a * a - 9 * b) * a / 27 + c;
+    var t = sqrt(q * q / 4 + p * p * p / 27);
+    return cbrt(t.sub(q / 2)).r + cbrt(t.neg().sub(q / 2)).r - a / 3;
+}
+function fun3(a, b, c, d) {
+    var k = 1 / a;
+    a = b * k;
+    b = c * k;
+    c = d * k;
+    var x = fun3_root(1, a, b, c);
+    show(x);
+    fun2(1, x + a, x * (x + a) + b);
+}
+
+function fun4_roots(a, b, c, d, e) {
+    var k = 1 / a;
+    a = b * k;
+    b = c * k;
+    c = d * k;
+    d = e * k;
+    var p, q, r, u, v1, v2, t;
+    p = b - 3 * a * a / 8;
+    q = a * (a * a - 4 * b) / 8 + c;
+    r = d - a * (a * (3 * a * a - 16 * b) + 64 * c) / 256;
+    if (equal(q, 0)) {
+        u = new Complex(0, 0);
+        v1 = sqrt(p * p - 4 * r).add(p).div(2);
+        v2 = v1.neg().add(p);
+    }
+    else {
+        u = sqrt(fun3_root(1, 2 * p, p * p - 4 * r, -q * q));
+        v1 = u.mul(u).sub(new Complex(q, 0).div(u)).add(p).div(2);
+        v2 = v1.add(new Complex(q, 0).div(u));
+    }
+    var list = [];
+    t = sqrt(u.mul(u).sub(v1.mul(4)));
+    list.push(u.neg().add(t).div(2).sub(a / 4));
+    list.push(u.neg().sub(t).div(2).sub(a / 4));
+    t = sqrt(u.mul(u).sub(v2.mul(4)));
+    list.push(u.add(t).div(2).sub(a / 4));
+    list.push(u.sub(t).div(2).sub(a / 4));
+    return list;
+}
+function fun4(a, b, c, d, e) {
+    var roots = fun4_roots(a, b, c, d, e);
+    for (let i = 0; i < roots.length; i++)
+        show(roots[i]);
+}
+
+function start(a, b, c, d, e, roots) {
+    roots.sort();
+    return fun5_calc(a, b, c, d, e, roots[0]) > 0 ? roots[0] - 0.5
+        : fun5_calc(a, b, c, d, e, roots[roots.length - 1]) < 0 ? roots[0] + 0.5
+            : (roots[1] + roots[2]) / 2;
+}
+const fun5_calc = (a, b, c, d, e, x) => x * (x * (x * (x * (x + a) + b) + c) + d) + e;
+const fun5_derivative = (a, b, c, d, x) => x * (x * (x * (5 * x + 4 * a) + 3 * b) + 2 * c) + d;
+function fun5(a, b, c, d, e, f) {
+    var k = 1 / a;
+    a = b * k;
+    b = c * k;
+    c = d * k;
+    d = e * k;
+    e = f * k;
+    var list = fun4_roots(5, 4 * a, 3 * b, 2 * c, d);
+    var roots = [];
+    for (let j = 0; j < list.length; j++)
+        if (equal(list[j].i, 0))
+            roots.push(list[j].r);
+    var t, x = 0;
+    var i = 0;
+    for (let j = 0; j < roots.length; j++)
+        if (equal(fun5_calc(a, b, c, d, e, roots[j]), 0)) {
+            x = roots[j];
+            i = -1;
+            break;
+        }
+    if (i == 0) {
+        if (roots.length > 0)
+            x = start(a, b, c, d, e, roots);
+        if (!equal(fun5_calc(a, b, c, d, e, x), 0))
+            do {
+                t = x;
+                x -= fun5_calc(a, b, c, d, e, x) / fun5_derivative(a, b, c, d, x);
+                i++;
+            } while (i < 6000 && Math.abs(x - t) > 1e-15);
+        show("迭代次数:" + i);
+    }
+    show(x);
+    fun4(1, x + a, x * (x + a) + b, x * (x * (x + a) + b) + c, x * (x * (x * (x + a) + b) + c) + d);
+}
+
+function show(text) {
+    var str = (typeof text == "number") ? Number(text.toFixed(12)) : text.toString();
+    document.getElementById("box").innerHTML += (str + "\\\\");
+}
 function calc(a) {
-    document.getElementById(a).value = (document.getElementById(a).value == "") ? "0" : eval(document.getElementById(a).value);
-    return Number(document.getElementById(a).value)
+    if (document.getElementById(a).value == "") document.getElementById(a).value = "0";
+    return Number(eval(document.getElementById(a).value));
 }
 
 function solve() {
     document.getElementById("box").innerHTML = "";
-    var a = calc("num5"),
-        b = calc("num4"),
-        c = calc("num3"),
-        d = calc("num2"),
-        e = calc("num1"),
-        f = calc("num0");
-    a != 0 ? fun5(b / a, c / a, d / a, e / a, f / a)
+    var a = calc("num5"), b = calc("num4"), c = calc("num3"),
+        d = calc("num2"), e = calc("num1"), f = calc("num0");
+    a != 0 ? fun5(a, b, c, d, e, f)
         : b != 0 ? fun4(b, c, d, e, f)
             : c != 0 ? fun3(c, d, e, f)
                 : d != 0 ? fun2(d, e, f)
@@ -107,237 +187,5 @@ function solve() {
                         : f != 0 ? show("无解")
                             : show("任意复数");
     box.innerHTML = '$$\\begin{array}{c}' + box.innerHTML + '\\end{array}$$';
-    MathJax.typeset()
-}
-
-function fun2(a, b, c) {
-    if (typeof c == "number") c = new Complex(c, 0);
-    var t = c.mul(-4 * a).add(b * b).sqrt();
-    show(t.neg().sub(b).div(2 * a));
-    show(t.sub(b).div(2 * a))
-}
-
-function fun3_one(a, b, c, d) {
-    var k = 1 / a;
-    a = b * k;
-    b = c * k;
-    c = d * k;
-    var e = (3 * b - a * a) / 9,
-        f = ((2 * a * a - 9 * b) * a / 27 + c) / 2;
-    var t = new Complex(f * f + e * e * e, 0).sqrt();
-    return t.sub(f).cbrt().add(t.neg().sub(f).cbrt()).r - a / 3
-}
-
-function fun3(a, b, c, d) {
-    var x = equal(d, 0) ? 0 : fun3_one(a, b, c, d);
-    show(x);
-    fun2(a, a * x + b, (a * x + b) * x + c)
-}
-
-function fun4(a, b, c, d, e) {
-    if (equal(e, 0)) {
-        show(0);
-        fun3(a, b, c, d)
-    } else {
-        var k = 1 / a;
-        a = b * k;
-        b = c * k;
-        c = d * k;
-        d = e * k;
-        if (equal(a, 0) && equal(c, 0)) {
-            var t = new Complex(b * b - 4 * d, 0).sqrt();
-            fun2(1, 0, t.add(b).div(2));
-            fun2(1, 0, t.neg().add(b).div(2))
-        } else {
-            var u = a / 2,
-                v = (4 * b - a * a) / 8;
-            if (c == 2 * u * v && d == v * v) {
-                fun2(1, u, v);
-                fun2(1, u, v);
-            } else {
-                var y = fun3_one(1, -b, a * c - 4 * d, 4 * b * d - a * a * d - c * c);
-                k = a * a / 4 - b + y;
-                if (equal(k, 0)) {
-                    var t = new Complex(y * y / 4 - d, 0).sqrt();
-                    fun2(1, a / 2, t.add(y / 2));
-                    fun2(1, a / 2, t.neg().add(y / 2))
-                } else {
-                    var p = Math.sqrt(k), q = (a * y - 2 * c) / (4 * p * p);
-                    fun2(1, a / 2 - p, y / 2 - p * q);
-                    fun2(1, a / 2 + p, y / 2 + p * q)
-                }
-            }
-        }
-    }
-}
-
-var root = new Array();
-
-function fun4_realroot(a, b, c, d, e) {
-    var y, p, q, t, delta;
-    t = 1 / a;
-    a = b * t;
-    b = c * t;
-    c = d * t;
-    d = e * t;
-    y = fun3_one(1, -b, a * c - 4 * d, 4 * b * d - a * a * d - c * c);
-    t = a * a / 4 - b + y;
-    if (equal(t, 0)) {
-        if (y * y - 4 * d >= 0) {
-            delta = a * a / 16 - y / 2 + Math.sqrt(y * y / 4 - d);
-            if (delta >= 0) {
-                t = Math.sqrt(delta);
-                root.push(-a / 4 + t);
-                root.push(-a / 4 - t)
-            }
-            delta = a * a / 16 - y / 2 - Math.sqrt(y * y / 4 - d);
-            if (delta >= 0) {
-                t = Math.sqrt(delta);
-                root.push(-a / 4 + t);
-                root.push(-a / 4 - t)
-            }
-        }
-    } else {
-        p = Math.sqrt(t);
-        q = (a * y - 2 * c) / (4 * p * p);
-        delta = 4 * p * (p + 4 * q - a) + a * a - 8 * y;
-        if (delta >= 0) {
-            t = Math.sqrt(delta);
-            root.push((2 * p - a + t) / 4);
-            root.push((2 * p - a - t) / 4)
-        }
-        delta = 4 * p * (p - 4 * q + a) + a * a - 8 * y;
-        if (delta >= 0) {
-            t = Math.sqrt(delta);
-            root.push((-2 * p - a + t) / 4);
-            root.push((-2 * p - a - t) / 4)
-        }
-    }
-}
-
-function start(a, b, c, d, e) {
-    for (let i = 0; i < root.length - 1; i++)
-        for (let j = 0; j < root.length - i - 1; j++)
-            if (root[j] > root[j + 1]) {
-                let t = root[j];
-                root[j] = root[j + 1];
-                root[j + 1] = t;
-            }
-    return fun5_calc(a, b, c, d, e, root[0]) > 0 ? root[0] - 0.5
-        : fun5_calc(a, b, c, d, e, root[root.length - 1]) < 0 ? root[0] + 0.5
-            : (root[1] + root[2]) / 2;
-}
-const fun5_calc = (a, b, c, d, e, x) => x * (x * (x * (x * (x + a) + b) + c) + d) + e;
-const fun5_derivative = (a, b, c, d, x) => x * (x * (x * (5 * x + 4 * a) + 3 * b) + 2 * c) + d;
-
-function fun5(a, b, c, d, e) {
-    if (equal(e, 0)) {
-        show(0);
-        fun4(1, a, b, c, d)
-    } else {
-        var i, x, t;
-        var f = true;
-        fun4_realroot(5, 4 * a, 3 * b, 2 * c, d);
-        for (i in root)
-            if (equal(fun5_calc(a, b, c, d, e, root[i]), 0)) {
-                f = false;
-                x = root[i];
-                break
-            }
-        if (f) {
-            x = root.length == 0 ? 0 : start(a, b, c, d, e);
-            i = 0;
-            do {
-                t = x;
-                x -= fun5_calc(a, b, c, d, e, x) / fun5_derivative(a, b, c, d, x);
-                i++
-            } while (i < 6000 && Math.abs(x - t) > 1e-15)
-            show("迭代次数:" + i);
-            t = fun5_calc(a, b, c, d, e, x);
-            if (Math.abs(t) > 1) show("误差较大");
-            show("L-R=" + t)
-        }
-        root.length = 0;
-        show(x);
-        fun4(1, x + a, x * (x + a) + b, x * (x * (x + a) + b) + c, x * (x * (x * (x + a) + b) + c) + d)
-    }
-}
-
-class Fraction {
-    constructor(a, b, c, d, e) {
-        this.A = "" + a;
-        this.B = (b == 1 && c != 1) ? "" : "" + b;
-        this.C = c == 1 ? "" : '\\sqrt{' + c + '}';
-        if (d > 0) {
-            this.D = (d == 1 && e != 1) ? '+' : '+' + d;
-        } else {
-            this.D = (d == -1 && e != 1) ? '-' : "" + d;
-        }
-        this.E = e == 1 ? "" : '\\sqrt{' + e + '}';
-    }
-    toString() {
-        var t = this.B + this.C + this.D + this.E;
-        return this.A == "1" ? '(' + t + ')' : '\\frac{' + t + '}{' + this.A + '}';
-    }
-}
-function isInteger(x) {
-    return Math.abs(x - Math.round(x)) < 1e-7;
-}
-function format(x) {
-    var a, b, c, d, f, t, y, z;
-    var str = "";
-    if (!isInteger(x) && Math.abs(x) < 25 && Math.abs(x) > 0.01) {
-        if (x < 0) {
-            str += "-";
-            x = -x;
-        }
-        for (a = 1; a < 25; a++) {
-            f = x * a;
-            if (isInteger(f)) {
-                str += '\\frac{' + Math.round(f) + '}{' + a + '}';
-                return str;
-            } else if (isInteger(f * f)) {
-                for (b = Math.floor(f); b > 1 && !isInteger(f * f / (b * b)); b--);
-                t = (b == 1 ? '' : b) + '\\sqrt{' + Math.round(f * f / (b * b)) + '}';
-                str += (a == 1) ? t : '\\frac{' + t + '}{' + a + '}';
-                return str;
-            }
-            else {
-                for (z = 1; z < 1000; z++) {
-                    y = Math.pow(f - Math.sqrt(z), 2);
-                    if (isInteger(y)) {
-                        for (b = Math.round(Math.sqrt(y)); b > 0; b--) {
-                            c = y / (b * b);
-                            if (isInteger(c)) {
-                                for (d = Math.round(Math.sqrt(z)); d > 0; d--) {
-                                    e = z / (d * d);
-                                    if (isInteger(e)) {
-                                        str += new Fraction(a, b, Math.round(c), d, Math.round(e));
-                                        return str;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    y = Math.pow(f + Math.sqrt(z), 2);
-                    if (isInteger(y)) {
-                        for (b = Math.round(Math.sqrt(y)); b > 0; b--) {
-                            c = y / (b * b);
-                            if (isInteger(c)) {
-                                for (d = Math.round(Math.sqrt(z)); d > 0; d--) {
-                                    e = z / (d * d);
-                                    if (isInteger(e)) {
-                                        str += new Fraction(a, b, Math.round(c), -d, Math.round(e));
-                                        return str;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    str += x;
-    return str;
-}
+    MathJax.typeset();
+}//1 2 3 
